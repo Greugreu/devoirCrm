d89738eb643e50f09ad9a2a2f6a7cecd5aae8aa8
Victor <victor.clarke@alphamaintenance-sa.fr>
2022-05-05 16:10:28 +0200

    remove unused using

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -15,7 +15,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Validator\Constraints\Json;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class ApiController extends AbstractController
