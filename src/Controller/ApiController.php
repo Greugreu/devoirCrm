b4bae15007d95a4ddd875df185b9aa1b051e4e98
Victor <victor.clarke@alphamaintenance-sa.fr>
2022-05-05 15:34:50 +0200

    getUserTodos

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -3,25 +3,34 @@
 namespace App\Controller;
 
 use App\Entity\Album;
+use App\Entity\Todo;
+use App\Entity\User;
 use App\Repository\AlbumRepository;
+use App\Repository\TodoRepository;
 use App\Repository\UserRepository;
 use App\services\ApiService\ApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class ApiController extends AbstractController
 {
     private ApiService $apiService;
     private UserRepository $userRepository;
     private AlbumRepository $albumRepository;
+    private ?User $currentUser;
+    private TodoRepository $todoRepository;
 
-    public function __construct(ApiService $apiService, UserRepository $userRepository, AlbumRepository $albumRepository)
+    public function __construct(ApiService $apiService, UserRepository $userRepository,
+                                AlbumRepository $albumRepository, TodoRepository $todoRepository)
     {
         $this->apiService = $apiService;
         $this->userRepository = $userRepository;
         $this->albumRepository = $albumRepository;
+        $this->todoRepository = $todoRepository;
+        $this->currentUser = $this->userRepository->findOneBy(['email' => $this->getUser()->getUserIdentifier()]);
     }
 
     #[Route('/api', name: 'app_api')]
@@ -32,15 +41,17 @@ class ApiController extends AbstractController
         ]);
     }
 
+    /**
+     * @throws TransportExceptionInterface
+     */
     #[Route('/api/getUserAlbums', name:'getAlbums')]
     public function getUserAlbums(): \Exception|Response
     {
-        $user = $this->userRepository->findOneBy(['email' => $this->getUser()->getUserIdentifier()]);
         $datas = $this->apiService->getApiData('users', 'albums');
         foreach ($datas as $data)
         {
             $album = new Album();
-            $album->setUserId($user);
+            $album->setUserId($this->currentUser);
             $album->setTitle($data['title']);
             try {
                 $this->albumRepository->add($album);
@@ -54,4 +65,30 @@ class ApiController extends AbstractController
             'data' =>  $this->albumRepository->findAll()
         ]);
     }
+
+    #[Route('/api/getUserTodos', name:'getUserTodos')]
+    public function getUserTodos()
+    {
+        $datas = $this->apiService->getApiData('users', 'todos');
+        foreach ($datas as $data)
+        {
+            $todo = new Todo();
+            $todo->setTitle($data['title']);
+            $todo->setUserId($this->currentUser);
+            $todo->setCompleted($data['completed']);
+
+            try {
+                $this->todoRepository->add($todo);
+            } catch (\Exception $exception) {
+                return $exception;
+            }
+        }
+
+        return $this->render('api/index.html.twig', [
+            'controller_name' => 'ApiController',
+            'data' =>  $this->todoRepository->findAll()
+        ]);
+    }
+
+
 }
