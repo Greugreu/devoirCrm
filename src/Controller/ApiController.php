7c9a9f8149fb12f3756a07e42f4b3eb873d40ffa
Victor <victor.clarke@alphamaintenance-sa.fr>
2022-05-05 15:37:10 +0200

    getUserPosts

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Album;
+use App\Entity\Post;
 use App\Entity\Todo;
 use App\Entity\User;
 use App\Repository\AlbumRepository;
+use App\Repository\PostRepository;
 use App\Repository\TodoRepository;
 use App\Repository\UserRepository;
 use App\services\ApiService\ApiService;
@@ -22,14 +24,17 @@ class ApiController extends AbstractController
     private AlbumRepository $albumRepository;
     private ?User $currentUser;
     private TodoRepository $todoRepository;
+    private PostRepository $postRepository;
 
     public function __construct(ApiService $apiService, UserRepository $userRepository,
-                                AlbumRepository $albumRepository, TodoRepository $todoRepository)
+                                AlbumRepository $albumRepository, TodoRepository $todoRepository,
+                                PostRepository $postRepository)
     {
         $this->apiService = $apiService;
         $this->userRepository = $userRepository;
         $this->albumRepository = $albumRepository;
         $this->todoRepository = $todoRepository;
+        $this->postRepository = $postRepository;
         $this->currentUser = $this->userRepository->findOneBy(['email' => $this->getUser()->getUserIdentifier()]);
     }
 
@@ -66,6 +71,9 @@ class ApiController extends AbstractController
         ]);
     }
 
+    /**
+     * @throws TransportExceptionInterface
+     */
     #[Route('/api/getUserTodos', name:'getUserTodos')]
     public function getUserTodos()
     {
@@ -89,6 +97,35 @@ class ApiController extends AbstractController
             'data' =>  $this->todoRepository->findAll()
         ]);
     }
+
+    /**
+     * @throws TransportExceptionInterface
+     */
+    #[Route('/api/getUserPosts', name:'getUserPosts')]
+    public function getUserPosts()
+    {
+        $datas = $this->apiService->getApiData('users', 'posts');
+        foreach ($datas as $data)
+        {
+            $post = new Post();
+            $post->setTitle($data['title']);
+            $post->setUserId($this->currentUser);
+            $post->setBody($data['body']);
+
+            try {
+                $this->postRepository->add($post);
+            } catch (\Exception $exception) {
+                return $exception;
+            }
+        }
+
+        return $this->render('api/index.html.twig', [
+            'controller_name' => 'ApiController',
+            'data' =>  $this->postRepository->findAll()
+        ]);
+    }
+
+
 
 
 }
